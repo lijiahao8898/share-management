5024dc91c29a489f7f9b00bb604dee475a7fb76e
lijiahao8898 <472239548@qq.com>
2018-03-13 20:51:41 +0800

    update cookie.js

--- app/src/util/cookie.js
+++ app/src/util/cookie.js
@@ -1,14 +1,18 @@
 export default {
+    // 添加cookie
     set: function (name, value, days) {
-        var d = new Date;
-        d.setTime(d.getTime() + 24 * 60 * 60 * 1000 * days);
+        var d = new Date();
+        var time = days ? 24 * 60 * 60 * 1000 * days : 24 * 60 * 60 * 1000;
+        d.setTime(d.getTime() + time);
         window.document.cookie = name + "=" + value + ";path=/;expires=" + d.toGMTString();
 
     },
+    // 获取cookie
     get: function (name) {
         var v = window.document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
         return v ? v[2] : null;
     },
+    // 删除cookie
     delete: function (name) {
         this.set(name, '', -1);
     }
